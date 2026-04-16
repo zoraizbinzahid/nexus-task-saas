37aee2d192b8f3edc1788c2c8bb5c1e94d00b382
Zoraiz Bin Zahid <zoraizbinzahid@gmail.com>
2026-04-17 02:38:11 +0500

    feat: connected WorkspaceViewSet to API router and verified security

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,28 +1,25 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from workspaces.views import WorkspaceViewSet
+
+# 1. Create the router
+router = DefaultRouter()
+
+# 2. Register the Workspace ViewSet
+router.register(r'workspaces', WorkspaceViewSet, basename='workspace')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     # This line connects all the Google login/signup magic
     path('accounts/', include('allauth.urls')),
 
+    # 3. This line connects your new Workspace API!
+    path('api/', include(router.urls)),
+    
     # These lines are for your regular Login/Logout API
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/auth/social/', include('allauth.socialaccount.urls')),
 ]

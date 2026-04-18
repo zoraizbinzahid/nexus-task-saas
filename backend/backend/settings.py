fc7a94f560653b2ef66b13f252969b8fee8f730f
Zoraiz Bin Zahid <zoraizbinzahid@gmail.com>
2026-04-19 02:24:39 +0500

    infra: docker containers verified and running

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,7 +59,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'backend.urls'
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # --- AUTH CONFIG ---
 AUTH_USER_MODEL = 'accounts.User'
